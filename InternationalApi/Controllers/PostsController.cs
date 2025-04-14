98ff24be22f37e4bcc10e339b8461d4007837737
Alex Ortega <mortegalex27@outlook.es>
2025-04-14 14:53:08 -0600

    Forma de realizar consultar en base al lenguaje

---
+++ InternationalApi/Controllers/PostsController.cs
@@ -0,0 +1,53 @@
+﻿
+using InternationalApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+
+namespace InternationalApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+
+        private readonly IStringLocalizer<PostsController> _stringLocalizer;
+        private readonly IStringLocalizer<SharedResource> _sharedResouceLocalizer;
+
+        public PostsController(IStringLocalizer<PostsController> stringLocalizer, IStringLocalizer<SharedResource> sharedResouceLocalizer)
+        {
+            _stringLocalizer = stringLocalizer;
+            _sharedResouceLocalizer = sharedResouceLocalizer;
+        }
+
+        [HttpGet]
+        [Route("PostControllerResource")]
+        public IActionResult GetUsingPostControllerResource()
+        {
+            // Find texto
+            var article = _stringLocalizer["Article"];
+            var postName = _stringLocalizer.GetString("Welcome").Value ?? String.Empty;
+
+            return Ok(new
+            {
+                PostType = article.Value,
+                PostName = postName
+            });
+        }
+
+        [HttpGet]
+        [Route("SharedResource")]
+        public IActionResult GetUsingSharedResources()
+        {
+            var article = _stringLocalizer["Article"];
+            var postName = _stringLocalizer.GetString("Welcome").Value ?? String.Empty;
+            var todayIs = string.Format(_sharedResouceLocalizer.GetString("TodayIs"), DateTime.Now.ToLongDateString());
+
+            return Ok(new
+            {
+                PostType = article.Value,
+                PostName = postName,
+                TodayIs = todayIs
+            });
+        }
+    }
+}
--- InternationalApi/Program.cs
+++ InternationalApi/Program.cs
@@ -2,16 +2,34 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// 1. LOCALIZATION
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// 2. SUPPORTED CULTURES
+var supportedCultures = new[] { "en-US", "es-ES", "fr-FR" }; // USA english, Spain Spanish and France french
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0]) // English by default
+    .AddSupportedCultures(supportedCultures) // Add al supported cultures
+    .AddSupportedUICultures(supportedCultures); // Add supported cultures to UI
+
+// 3. ADD LOCALIZATION to App
+app.UseRequestLocalization(localizationOptions);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI( // url = /swagger
+        options => options.SwaggerEndpoint("/openapi/v1.json", "International API v1")
+    );
 }
 
 app.UseHttpsRedirection();
