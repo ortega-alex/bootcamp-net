9ed8e4134b811032a4884b0cc315e07bf7c13a79
Alex Ortega <mortegalex27@outlook.es>
2025-04-15 12:28:47 -0600

    Configuracion de log a nivel entity frame

--- UniversitiApiBackend/DataAccess/UniversityDBContext.cs
+++ UniversitiApiBackend/DataAccess/UniversityDBContext.cs
@@ -5,8 +5,10 @@ namespace UniversitiApiBackend.DataAccess
 {
     public class UniversityDBContext : DbContext
     {
-        public UniversityDBContext(DbContextOptions<UniversityDBContext> options) : base(options)
+        private readonly ILoggerFactory _loggerFactory;
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options, ILoggerFactory loggerFactory) : base(options)  
         {
+            _loggerFactory = loggerFactory;
         }
 
         // TODO: Add DbSet (Tables of our Data base)
@@ -14,6 +16,19 @@ namespace UniversitiApiBackend.DataAccess
         public DbSet<Course>? Course { get; set; }
         public DbSet<Chapters>? Indexes { get; set; } 
         public DbSet<Category>? Categories { get; set; }
-        public DbSet<Student>? Students { get; set; } 
+        public DbSet<Student>? Students { get; set; }
+
+
+        // cada que existe un cambio en el modelo quedara persistida
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var logger = _loggerFactory.CreateLogger<UniversityDBContext>();
+            //optionsBuilder.LogTo(d => logger.Log(LogLevel.Information, d, new[] { DbLoggerCategory.Database.Name }));
+            //optionsBuilder.EnableSensitiveDataLogging(); 
+                       
+            optionsBuilder.LogTo(d => logger.Log(LogLevel.Information, d, new[] { DbLoggerCategory.Database.Name }), LogLevel.Information)  // filtar datos que sea de nivel informativo
+                .EnableSensitiveDataLogging() // para que guarde los datos sensibles en la base de datos
+                .EnableDetailedErrors(); // si hay error que sean lo mas detallados posibles
+        }
     }
 }
